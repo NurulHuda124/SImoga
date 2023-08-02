37715e56900ad79f3a847ce4f880a9071bc1345e
Guntur <bagus.aryoga46@gmail.com>
2023-08-02 11:32:06 +0700

    Fix LineChart

--- app/Filament/Widgets/Dashboard/PegawaiBarChart.php
+++ app/Filament/Widgets/Dashboard/PegawaiBarChart.php
@@ -15,6 +15,7 @@ class PegawaiBarChart extends BarChartWidget
                 [
                     'label' => 'Jumlah Pegawai',
                     'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    'fill' => 'start',
                 ],
             ],
             'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
