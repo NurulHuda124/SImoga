66d51c4f5edbae91d11bb6488e2ffde5c4f14899
NurulHuda124 <rulhuda313@gmail.com>
2023-08-02 16:13:17 +0700

    barchart selesai

--- app/Filament/Widgets/Dashboard/PegawaiBarChart.php
+++ app/Filament/Widgets/Dashboard/PegawaiBarChart.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets\Dashboard;
 
+use App\Models\Kontrak;
 use Filament\Widgets\BarChartWidget;
 
 class PegawaiBarChart extends BarChartWidget
@@ -10,15 +11,27 @@ class PegawaiBarChart extends BarChartWidget
     protected static ?int $sort = 2;
     protected function getData(): array
     {
+        $jmlhBerlaku= Kontrak::where('status_kontrak', 'Berlaku')->count('status_kontrak');
+        $jmlhTdkBerlaku = Kontrak::where('status_kontrak', 'Tidak Berlaku')->count('status_kontrak');
         return [
             'datasets' => [
                 [
                     'label' => 'Jumlah Pegawai',
-                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    'data' => [$jmlhBerlaku, $jmlhTdkBerlaku],
                     'fill' => 'start',
+                    'backgroundColor' => [
+                    'rgba(255, 205, 86, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    ],
+                    'borderColor' => [
+                    'rgb(255, 205, 86)',
+                    'rgb(54, 162, 235)',
+                    ],
+                    'borderWidth' => 3,
+                    'borderRadius' => 10,
                 ],
             ],
-            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            'labels' => ['Pegawai Kontrak Berlaku', 'Pegawai Kontrak Tidak Berlaku'],
         ];
     }
 }
