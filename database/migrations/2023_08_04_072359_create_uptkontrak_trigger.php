f1ca7316e84af62ac559c57d72a1d2f8b623f7b4
NurulHuda124 <rulhuda313@gmail.com>
2023-08-04 14:41:39 +0700

    membuat trigger untuk update, delete table kontrak dan pensiun

---
+++ database/migrations/2023_08_04_072359_create_uptkontrak_trigger.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('
+        CREATE TRIGGER uptkontrak_trigger AFTER UPDATE ON pegawais
+        FOR EACH ROW
+        BEGIN
+        UPDATE kontraks
+        SET
+        nama_pegawai = NEW.nama_pegawai,
+        email = NEW.email,
+        tanggal_kontrak_awal = NEW.tanggal_kontrak_awal,
+        tanggal_kontrak_akhir = NEW.tanggal_kontrak_akhir,
+        status_kontrak = NEW.tanggal_kontrak_akhir
+        WHERE kontraks.nama_pegawai = NEW.nama_pegawai;
+        END;
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS uptkontrak_trigger');
+    }
+};
