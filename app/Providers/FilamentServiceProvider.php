4b92b2f8e0a331918b0e88fa572cb410563f4114
Guntur <bagus.aryoga46@gmail.com>
2023-08-11 08:12:57 +0700

    change theme pages

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Filament\Facades\Filament;
 use Illuminate\Foundation\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -10,15 +9,6 @@ class FilamentServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Filament::serving(function () {
-        // Using Vite
-        Filament::registerViteTheme('resources/css/filament.css');
-
-        // Using Laravel Mix
-        Filament::registerTheme(
-        app(Vite::class)('resources/css/filament.css'),
-        );
-        });
-
+        //
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,20 @@
-import colors from "tailwindcss/colors";
-import forms from "@tailwindcss/forms";
-import typography from "@tailwindcss/typography";
-
+const colors = require('tailwindcss/colors')
 export default {
-    content: [
-        "./resources/**/*.blade.php",
-        "./vendor/filament/**/*.blade.php"
-    ],
-    darkMode: "class",
+    content: ['./resources/**/*.blade.php', './vendor/filament/**/*.blade.php'],
+    darkMode: 'class',
     theme: {
         extend: {
             colors: {
                 danger: colors.rose,
                 primary: colors.blue,
+                secondary: colors.gray,
                 success: colors.green,
                 warning: colors.yellow,
             },
         },
     },
-    plugins: [forms, typography],
-};
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+}
